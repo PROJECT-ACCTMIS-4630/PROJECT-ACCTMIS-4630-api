95fe3005aeee5f0af4c31171404275d6d70ee811
Tihtina Masebo <masebo.1@osu.edu>
2026-03-31 00:47:48 -0400

    Part 2 - Data Access with EF Core and SQLite #7

--- PROJECT-ACCTMIS-4630.Api/Program.cs
+++ PROJECT-ACCTMIS-4630.Api/Program.cs
@@ -1,13 +1,21 @@
+using PROJECT_ACCTMIS_4630.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddDbContext<StoreContext>(options =>
+    options.UseSqlite("Data Source=../Registrar.sqlite")
+);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.MapControllers();
--- PROJECT-ACCTMIS-4630.Domain/Catalog/Rating.cs
+++ PROJECT-ACCTMIS-4630.Domain/Catalog/Rating.cs
@@ -2,6 +2,7 @@ namespace PROJECT_ACCTMIS_4630.Domain.Catalog;
 
 public class Rating
 {
+    public int Id { get; set; }
     public int Stars { get; set; }
     public string UserName { get; set; }
     public string Review { get; set; }
