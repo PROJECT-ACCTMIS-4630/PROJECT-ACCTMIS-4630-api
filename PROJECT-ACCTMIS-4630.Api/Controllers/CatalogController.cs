fffc0cbfb579577ea9f95d43688ffc065a0fdc1d
Thomas Life <life.8@osu.edu>
2026-03-30 22:05:59 -0400

    Added all 6 routes for catalog api

--- PROJECT-ACCTMIS-4630.Api/Controllers/CatalogController.cs
+++ PROJECT-ACCTMIS-4630.Api/Controllers/CatalogController.cs
@@ -25,5 +25,29 @@ namespace PROJECT_ACCTMIS_4630.Api.Controllers
             item.Id = id;
             return Ok(item);
         }
+        [HttpPost]
+        public IActionResult Post(Item item)
+        {
+            return Created("/catalog/42", item);
+        }
+        [HttpPost("{id:int}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating)
+        {
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.Id = id;
+            item.AddRating(rating);
+            return Ok(item);
+        }
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Item item)
+        {
+            return NoContent();
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+
     }
 }
