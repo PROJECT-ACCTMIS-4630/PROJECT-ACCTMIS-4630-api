4f35e766410f6dc34133027d9bb42b67f1b5012a
Tihtina Masebo <masebo.1@osu.edu>
2026-04-26 19:16:59 -0400

    Add CORS support

--- PROJECT-ACCTMIS-4630.Api/Program.cs
+++ PROJECT-ACCTMIS-4630.Api/Program.cs
@@ -7,6 +7,15 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlite("Data Source=../Registrar.sqlite")
 );
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,7 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseHttpsRedirection();
+app.UseCors();
 app.MapControllers();
 
 app.Run();
